487f1ac7e337550ca204719092da23263de8be6f
10aded <10aded-Streaming@protonmail.com>
2025-03-06 11:19:55 +1000

    started making dwarf planet grayscale

--- dwarf_planet/fragment_shader.glsl
+++ dwarf_planet/fragment_shader.glsl
@@ -2,11 +2,26 @@
 // Version above obtained by looking at the table at:
 // https://en.wikipedia.org/w/index.php?title=OpenGL_Shading_Language&oldid=1270723503
 
+precision lowp float;
+
 varying lowp vec2 TexCoord;
 
 uniform sampler2D uSampler;
 
+
+const float lambda = 1.0;
+
+mat3 lambda_to_matrix(float l) {
+    float norm = 1.0 / (1.0 + 2.0 * l);
+    return norm * mat3(1, l, l, l, 1, l, l, l, 1);
+}
+
+
 void main() {
-  lowp vec4 texture_color = texture2D(uSampler, TexCoord);
-  gl_FragColor = texture_color;
+  mat3 lmat = lambda_to_matrix(lambda);
+  
+  vec4 texture_color = texture2D(uSampler, TexCoord);
+
+  vec3 lcolor = lmat * texture_color.xyz;
+  gl_FragColor = vec4(lcolor, 1);
 }
